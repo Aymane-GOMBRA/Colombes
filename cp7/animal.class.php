56ce117d68ab904e4c33931198422388a2fb32b6
Aymane-GOMBRA <agombra95@gmail.com>
2021-02-09 16:56:17 +0100

    17 animal

--- cp7/animal.class.php
+++ cp7/animal.class.php
@@ -9,19 +9,87 @@ class Animal
     private $color='';
     private $weight=0.0;
 
+    //Constantes de classe
+    const TYPE_AIR = 'aérien';
+    const TYPE_WATER = 'marin';
+    const TYPE_GROUND = 'terrestre';
+    const TYPE_ELSE = 'autre';
+
+    //Constructeur de la classe
+    public function __construct(string $newName='', string $newType=self::TYPE_ELSE, string $newColor='blanc', float $newWeight=0.02){
+        //Assigne les valeurs des arguments aux attributs
+        $this->name=$newName;
+        $this->type=$newType;
+        $this->color=$newColor;
+        $this->weight=$newWeight;
+    }
+
     //Accesseur/Mutateurs (Getters/Setters)
     public function getType():string
     {
         return $this->type;
     }
-    public function setType(string $newType){
-        $this->type = $newType;
+    public function setType(string $newType)
+    {
+        $newType=strtolower($newType);
+        if($newType===self::TYPE_GROUND || $newType===self::TYPE_AIR || $newType===self::TYPE_WATER){
+            $this->type = $newType;
+        }else{
+            $this->type = self::TYPE_ELSE;
+        }
     }
+
+    public function getColor():string
+    {
+        return $this->color;
+    }
+    public function setColor(string $newColor)
+    {
+        $this->color = $newColor;
+    }
+
+    public function getWeight():float
+    {
+        return $this->weight;
+    }
+    public function setWeight(string $newWeight)
+    {
+        //Le poids doit être compris entre 20g et 1100kg
+        if($newWeight >= 0.02 && $newWeight<=1100){
+            $this->weight = $newWeight;
+        }else{
+            throw new Exception('Le poids doit être compris entre 20g et 1100kg');
+        }
+    }
+
 
     //Méthode publique pour se déplacer
     public function move(): string
     {
-        return 'Je me deplace';
+        $type=$this->getType();
+        switch($type){
+            case self::TYPE_WATER:
+                return 'Je nage';
+                break;
+            case self::TYPE_GROUND:
+                return 'Je marche';
+                break;
+            case self::TYPE_AIR:
+                return 'Je vole';
+                break;
+            default:
+            return 'Je me déplace';
+        }
+    }
+
+    public function eat(Animal $prey){
+        //Un animal mange un autre animal : 
+
+        //le prédateur gagne le poids de la proie
+        $this->weight += $prey->getWeight();
+        // la proie perd son poids
+        $prey->weight=0;
+        
     }
 }
 ?>
--- cp7/test_animal.php
+++ cp7/test_animal.php
@@ -4,6 +4,42 @@ echo '<h2>Test 1 : Instanciation</h2>';
 $obj1 = new Animal;
 $obj1->name='Milou';//valorisation attribut
 
-var_dump($obj1);
 echo '<p>'.$obj1->move();//appel méthode
+
+echo '<h2>Test 2 : Utilisation des accesseurs</h2>';
+$obj2 = new Animal;
+$obj2->name='Snoopy';//valorisation attribut
+$obj2->setType('Terrestre');
+echo '<p>'.$obj2->name.': Type = '.$obj2->getType();
+
+$obj3 = new Animal;
+$obj3->name='Dingo';//valorisation attribut
+$obj3->setType('Terrestre');
+$obj3->setColor('Bleu');
+echo '<p>'.$obj3->name.': Type = '.$obj3->getType().', Couleur = '.$obj3->getColor();
+
+$obj4 = new Animal;
+$obj4->name='Toutou';//valorisation attribut
+$obj4->setType('Terrestre');
+$obj4->setColor('Rose');
+$obj4->setWeight(1100.0);
+echo '<p>'.$obj4->name.': Type = '.$obj4->getType().', Couleur = '.$obj4->getColor().', Poids =  '.$obj4->getWeight().' kg';
+
+echo '<h2> Test 3 : Utilisation des constantes</h2>';
+echo '<p>'.Animal::TYPE_GROUND;
+
+echo '<h2> Test 4 : Utilisation des constructeurs</h2>';
+$obj5 = new Animal('Garfield', Animal::TYPE_GROUND, 'Orange et noir',7.8);
+var_dump($obj5);
+
+echo '<h2> Test 5 : Appel des méthodes</h2>';
+$obj6=new Animal('Titi', Animal::TYPE_AIR, 'Jaune', .3);
+$obj7=new Animal('Sylvestre', Animal::TYPE_GROUND, 'Noir et Blanc', 5.25);
+echo '<p>'.$obj6->move();
+echo '<p>'.$obj7->move();
+var_dump($obj6);
+var_dump($obj7);
+$obj7->eat($obj6);
+var_dump($obj6);
+var_dump($obj7);
 ?>
